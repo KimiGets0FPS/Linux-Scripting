ff9a01d1cde277226f649d05d27bc7796cd3f15b
Zhewei Wan <zheweiwan@gmail.com>
2024-11-15 16:40:19 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -10,18 +10,35 @@ _password = getpass.getpass()
 def main() -> None:
     run_command("sudo su")
     ufw()
+    update()
+    remove_unauthorized_users()
+    create_new_users()
 
 
 def ufw() -> None:
+    """
+    Installs ufw if it isn't already installed
+    Enables ufw and checks if it is running
+    """
     run_commands(["apt-get install ufw", "ufw enable", "ufw status"])
+    print("ufw installed and enabled")
 
 def update() -> None:
-    run_commands(["apt update -y", "apt upgrade -y"]) # Updates
-    run_commands(["apt-get install unattended-upgrades -y"]) # Auto-Updates
+    """
+    Running update and upgrade to update the system
+    Getting unattended-upgrades to enable automatic updates
+    """
+    run_commands(["apt update -y", "apt upgrade -y"])
+    print_green("System updated")
+    run_commands(["apt-get install unattended-upgrades -y", "systemctl start unattended-upgrades"])
+    print_green("Auto updates started")
+
 
 def remove_unauthorized_users() -> None:
     ...
 
+def create_new_users():
+    ...
 
 if __name__ == "__main__":
     main()
