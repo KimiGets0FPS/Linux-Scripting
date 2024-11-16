e49cac29625f855907277c93869e27fddb2a08c9
Zhewei Wan <zheweiwan@gmail.com>
2024-11-15 17:45:10 -0800

    Dinner Break

--- customfunctions.py
+++ customfunctions.py
@@ -2,38 +2,63 @@ import os
 import subprocess
 
 
-
 def _get_command(command: str) -> list[str]:
     return command.split(" ")
 
+
 def run_command(command: str) -> None:
     subprocess.run(_get_command(command), capture_output=True, text=True)
+
 
 def run_commands(commands: list[str]) -> None:
     for i in range(len(commands)):
         run_command(commands[i])
 
-def clear():
+
+def confirmation(clear=True):
+    input("Press ENTER to continue...")
+    if clear:
+        _clear()
+
+
+def _clear():
     os.system("clear")
     exit(1)
 
-# DECORATION
 
-def print_green(string: str) -> None:
-    print(f"\033[92m{string}\033[0m")
+# Decoration
 
-def print_yellow(string: str) -> None:
-    print(f"\033[93m{string}\033[0m")
-
-def print_red(string: str) -> None:
-    print(f"\033[91m{string}\033[0m")
-
-def print_blue(string: str) -> None:
-    print(f"\033[94m{string}\033[0m")
+def print_red(string: str, bold: bool = False, underline: bool = False) -> None:
+    decoration = ""
+    if bold:
+        decoration += "1;"
+    if underline:
+        decoration += "4;"
+    print(f"\033[{decoration}91m{string}\033[0m")
 
 
-def main():
-    ...
+def print_green(string: str, bold: bool = False, underline: bool = False) -> None:
+    decoration = ""
+    if bold:
+        decoration += "1;"
+    if underline:
+        decoration += "4;"
+    print(f"\033[{decoration}92m{string}\033[0m")
 
-if __name__ == "__main__":
-    main()
+
+def print_yellow(string: str, bold: bool = False, underline: bool = False) -> None:
+    decoration = ""
+    if bold:
+        decoration += "1;"
+    if underline:
+        decoration += "4;"
+    print(f"\033[{decoration}93m{string}\033[0m")
+
+
+def print_blue(string: str, bold: bool = False, underline: bool = False) -> None:
+    decoration = ""
+    if bold:
+        decoration += "1;"
+    if underline:
+        decoration += "4;"
+    print(f"\033[{decoration}924m{string}\033[0m")
--- main.py
+++ main.py
@@ -3,44 +3,119 @@ import os
 from customfunctions import *
 
 
-_username = getpass.getuser()
-_password = getpass.getpass()
-
 def main():
-    ufw()
-    update()
-    remove_unauthorized_users()
-    create_new_users()
+
+    ufw()  # Gets Firewall
+
+    update()  # Updating system
+
+    change_password(input("Enter a password (Default: CyberPatriot123!@#): "))  # Changing user passwords
+
+    remove_unauthorized_users()  # Removes unauthorized users
+
+    create_new_users()  # Creates new users
 
 
-def ufw():
+def ufw() -> None:
     """
     Installs ufw if it isn't already installed
+
     Enables ufw and checks if it is running
+
+    :return: None
     """
     run_commands(["apt-get install ufw", "ufw enable", "ufw status"])
     print("ufw installed and enabled")
 
-def update():
+    confirmation()
+
+def update() -> None:
     """
     Running update and upgrade to update the system
+
     Getting unattended-upgrades to enable automatic updates
+
+    :return: None
     """
     print_yellow("Updating system...")
-    print_red("DO NOT STOP THE SCRIPT")
     run_commands(["apt update -y", "apt upgrade -y"])
     print_green("System updated")
+
+    confirmation(False)
 
     print_yellow("Installing unattended-upgrades...")
     run_commands(["apt-get install unattended-upgrades -y", "systemctl start unattended-upgrades"])
     print_green("Auto updates started")
 
+    confirmation()
+
+def change_password(password: str="CyberPatriot123!@#") -> None:
+    """
+    :param password: Password to change to (Default: CyberPatriot123!@#)
+
+    :return: None
+    """
 
 def remove_unauthorized_users() -> None:
-    ...
+    """
+    Removes unauthorized users not listed in normal_users.txt
 
-def create_new_users():
-    ...
+    Valid users are provided in the CyberPatriot ReadMe file
+
+    :return: None
+    """
+    normal_users = open("normal_users.txt", "r").read().splitlines()
+    sudoers = open("admins.txt", "r").read().splitlines()
+
+    if not normal_users or not sudoers:
+        print_red("ADD USERS TO normal_users.txt AND admins.txt BEFORE DOING THIS COMMAND", bold=True)
+        confirmation()
+        return
+
+    confirmation()
+
+def create_new_users() -> None:
+    """
+    Creates users from normal_users.txt
+    """
+    new_users = open("new_users.txt", "r").read().splitlines()
+
+    confirmation()
+
+def secure_root() -> None:
+    """
+    Secures Root
+
+    :return: None
+    """
+    print_yellow("Secure Root...")
+    run_command("sudo passwd -l root")
+    print_green("Root Secured")
+
+    confirmation()
+
+def secure_shadow() -> None:
+    """
+    Securing /etc/shadow
+
+    :return: None
+    """
+    run_commands(["sudo chmod 640 /etc/shadow", "ls -l /etc/shadow"])
+
+    confirmation()
+
+def remove_hacking_tools() -> None:
+    """
+    Removes all hacking tools
+
+    :return: None
+    """
+
+    confirmation()
+
 
 if __name__ == "__main__":
+    _username = getpass.getuser()
+    _password = getpass.getpass()
+
     main()
